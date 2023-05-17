2fa69b31b0d4d8d73fb3c5a3db701c1c563179f0
Joost van Dorp <joostvandorp@gmail.com>
2023-05-17 15:23:51 -0300

    wip

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,57 @@
 import * as Phaser from 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+class PreloadScene extends Phaser.Scene {
+    constructor() {
+        super('PreloadScene');
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    preload() {
+        // Display a loading message
+        const loadingText = this.add.text(256, 256, 'Loading...', {
+            fontFamily: 'Arial',
+            fontSize: '24px',
+            color: '#ffffff',
+        });
+        loadingText.setOrigin(0.5);
+        
+        // Preload your game assets here
+        // For example, load an image
+        this.load.image('tiles', 'assets/images/tileset.png');
+        this.load.tilemapTiledJSON('map', 'assets/images/map.json');
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    create() {
+        // Transition to the main game scene or any other scene
+        this.scene.start('MainScene');
     }
 }
 
+// Define your main game scene
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super('MainScene');
+    }
+
+    create() {
+        // Add your game logic here
+        // You can access the loaded assets using this.textures or this.cache
+        const map = this.make.tilemap({key: 'map'});
+        const tileset = map.addTilesetImage('tileset', 'tiles', 32,32,0,0);
+        const layer1 = map.createLayer('Tile Layer 1', tileset,0,0);
+    }
+}
+
+// Create the Phaser game configuration
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    backgroundColor: '#333333',
+    width: 640,
+    height: 640,
+    scale: {
+        zoom: 1
+    },
+    scene: [PreloadScene, MainScene],
 };
 
+// Instantiate the Phaser game
 const game = new Phaser.Game(config);
